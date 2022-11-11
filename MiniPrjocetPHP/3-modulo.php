68a6e7475dae92207df4e5f1c838444bc787a8a6
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-11-11 15:20:24 +0100

    Update 3-modulo.php

--- MiniPrjocetPHP/3-modulo.php
+++ MiniPrjocetPHP/3-modulo.php
@@ -1,6 +1,14 @@
 <?php
 
-$num1 = rand(10, 100);
-$num2 = rand(10, 120);
+(int)$num1 = rand(1, 100); 
+(int)$num2 = rand(1, 100);
 
+echo $num1 . " " . $num2 . "\n";
 
+function findRemainder($num, $devisor)
+{
+    $remainder = ($num - $devisor * (int)($num / $devisor)); 
+    return $remainder;
+}
+
+echo findRemainder($num1, $num2);
