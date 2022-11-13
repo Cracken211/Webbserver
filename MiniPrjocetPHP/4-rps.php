e848aa8d9fbfed2ecf9bceed80d370d4ba508f4d
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-11-13 16:31:32 +0100

    ehhh

---
+++ MiniPrjocetPHP/4-rps.php
@@ -0,0 +1,75 @@
+<?php
+function rpsUser ($userThrow)
+{
+    $userThrow = strtolower(readline("Please enter your desired input. Rock, Paper och Scissors >> "));
+
+    if ($userThrow == "Rock")
+    {
+        $userTrow = 0;
+    }
+    elseif($userThrow == "Paper")
+    {
+        $userTrow = 1;
+    }
+    elseif($userThrow == "Scissors")
+    {
+        $userTrow = 2;
+    }
+    else
+    {
+        echo "Invalid answer";
+    }
+    return $userThrow;
+}
+
+function rps ($userInpt, $genInpt)
+{
+    if ($genInpt == $userInpt) 
+        {
+        $outcome = "Drew...";
+        }
+        
+        elseif ($genInpt == 0 && $userInpt == 1) 
+        {
+        $outcome = "Win!";
+        }
+        
+        elseif ($genInpt == 1 && $userInpt == 2) 
+        {
+        $outcome = "Win!";
+        }
+        
+        elseif ($genInpt == 2 && $userInpt == 0) 
+        {
+        $outcome = "Win!";
+        }
+        
+        elseif ($genInpt == 2 && $userInpt == 1) 
+        {
+        $outcome = "Lost...";
+        }
+        
+        elseif ($genInpt == 1 && $userInpt == 0) 
+        {
+        $outcome = "Lost...";
+        }
+        
+        elseif ($genInpt == 0 && $userInpt == 2) 
+        {
+        $outcome = "Lost...";
+        }
+        else
+        {
+            $outcome = "VAR_Why did this become an error...";
+            echo "Echo_Why did this become an error...";
+        }
+
+        echo "You $outcome";
+};
+
+$genInpt = rand(0,2);
+echo "$genInpt\n"; // Bara så att jag kan see att det fungerar... 
+
+
+rpsUser();
+rps($genInpt, $userThrow);
