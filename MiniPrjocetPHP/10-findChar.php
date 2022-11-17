7caa443508482c9e733ca1f36d7f2c6c2fc80421
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-11-17 21:43:23 +0100

    Add files via upload

---
+++ MiniPrjocetPHP/10-findChar.php
@@ -0,0 +1,17 @@
+<?php
+
+function getChar($string){
+
+$string = explode(" ", $string);
+for ($i = 0; $i <= count($string); $i++){
+    $stringOne = $string[$i];
+    $stringTwo = $stringOne[0];
+
+    if($stringTwo == "d" || $stringTwo == "D"){
+        echo "$stringOne\n";
+    }
+}
+}
+
+$sentence = "dood You're good at English when you know the difference between a man eating chicken and a man-eating chicken dawg";
+getChar($sentence);
