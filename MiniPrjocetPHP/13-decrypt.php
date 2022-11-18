2c80f50f39294975570ae523b5929cd232d6d765
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-11-18 13:11:39 +0100

    Add files via upload

---
+++ MiniPrjocetPHP/13-decrypt.php
@@ -0,0 +1,10 @@
+<?php
+
+function decrypt($string){
+    $string = explode(" ", $string );
+
+
+
+}
+
+decrypt("D E F G H I J K L M N O P Q R S T U V W X Y Z A B C"); // ABC last 3 from back, use to 
