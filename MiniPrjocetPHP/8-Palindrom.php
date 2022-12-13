748ff5ce57448dc0a9c82d634ba4d45075900f2f
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-12-13 20:19:20 +0100

    Update 8-Palindrom.php

--- MiniPrjocetPHP/8-Palindrom.php
+++ MiniPrjocetPHP/8-Palindrom.php
@@ -1,8 +1,25 @@
 <?php
 
+function checkPalindrom($string)
+{
+    $stringReverse = strrev($string);
+    if($string == $stringReverse){
+        echo "Your string is palindrom!\n";
+    } else{
+        echo "Your string is not palindrom...\n";
+    }   
+}
+
+echo "Check if your string is palindrom\n";
+$stringUser = readline("Enter string here >> ");
+
+checkPalindrom($stringUser);
+
+// ELLER! 
+
 function isPalindrome($str) {
 
-    $str = preg_replace('/[^A-Za-z0-9]/', '', strtolower($str)); // De de good remover of the non good looking letters yes :) Finally found function. 
+    $str = preg_replace('/[^A-Za-z0-9]/', '', strtolower($str)); 
     return $str == strrev($str);
 }
 
