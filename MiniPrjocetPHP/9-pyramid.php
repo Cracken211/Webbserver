3d4289472e37cbdc08bab01e293999ab4255832a
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2022-12-16 09:41:56 +0100

    Update 9-pyramid.php

--- MiniPrjocetPHP/9-pyramid.php
+++ MiniPrjocetPHP/9-pyramid.php
@@ -4,16 +4,18 @@ function pyramid($n)
     $k = $n * 2;
 
     for ($i = 0; $i < $n; $i++){
+
         for ($j = 0; $j < $k; $j++){
             echo " ";
         }
         $k -= 1;
-        for ($j = 0; $j <= $i; $j++ ){
-            echo " #";
+        $new = $i * 2 + 1;
+        for ($j = 0; $j < $new; $j++ ){
+            echo "#";
         }
         echo "\n";
     }
 }
 
-$n = 20;
+$n = 10; 
 pyramid($n);
