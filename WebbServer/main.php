2eedee8eadcdf2053c239d1b047b28d63ca62a1d
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2023-01-26 22:03:51 +0100

    Add files via upload

--- WebbServer/main.php
+++ WebbServer/main.php
@@ -1,65 +1,53 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>AgeCalc</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mega homapage</title>
 </head>
 
-
-
-
-
-
 <body>
+    <header>
+        <nav class="container">
+            <a href="?page=calc">Calc</a>
+            <a href="?page=ageCalc">AgeCalc</a>
+        </nav>
+    </header>
 
-    <li>
-        <a href="?AgeCalc">Claculator</a>
-        <a href="?NumCalc">Age Calculator</a>
-    </li>
+        <?php
+            $calculator = '
+            <form>  
+                <label>First num </label> <br>  
+                <input type="text" placeholder="305"> <br> <br>  
+                <label>Second num</label> <br>  
+                <input type="text" placeholder="Ex. 432">  <br> <br>  
+                <input type="submit" value="Submit">  
+            </form>  
+            ';
 
+            $ageCalculator = '    
+            <form>  
+                <label>Enter Age </label> <br>  
+                <input type="text" placeholder="21"> <br> <br>  
+                <label>Enter Year</label> <br>  
+                <input type="text" placeholder="1993">  <br> <br>  
+                <input type="submit" value="Submit">  
+            </form>  
+            ';
 
-    <div class="wrapper-form">
-        <form>  
-        <label>Enter Age </label> <br>  
-            <input type="text" placeholder="30"> <br> <br>  
-        <label>Enter Year</label> <br>  
-            <input type="Birth Year" placeholder="Ex. 1993">  <br> <br>  
-            <input type="submit" value="Submit">  
-        </form>  
-    </div>
-
-
-
-
-<?php
-
-if($_GET["AgeCalc"]){
-    ?>
-       <div class="wrapper-form">
-        <form>  
-        <label>Enter Age </label> <br>  
-            <input type="text" placeholder="30"> <br> <br>  
-        <label>Enter Year</label> <br>  
-            <input type="Birth Year" placeholder="Ex. 1993">  <br> <br>  
-            <input type="submit" value="Submit">  
-        </form>  
-    </div>
-
-<?php
-}
-elseif($_GET["NumCalc"]){
-    echo "Hello";
+    if($_GET["page"] == "calc"){
+        echo $calculator;
 
 
-}
-// elseif ($_GET[]??{
-//     echo "Page not found...";
-// }
+    }elseif($_GET["page"] == "ageCalc"){
+        echo $ageCalculator;
 
-?>
+    }else{
+        echo "No page found...";
+    }
+
+        ?>
 </body>
 </html>
