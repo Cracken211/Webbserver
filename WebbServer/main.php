746bcca99a0a73bc869fc5b70196d8a3ad48d936
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2023-01-26 22:31:52 +0100

    Cringe.php
    noob

--- WebbServer/main.php
+++ WebbServer/main.php
@@ -18,35 +18,54 @@
 
         <?php
             $calculator = '
-            <form>  
+            <form method="get">  
+
                 <label>First num </label> <br>  
-                <input type="text" placeholder="305"> <br> <br>  
+                <input type="text" name="num1" placeholder="Ex. 305"> <br> <br> 
+
                 <label>Second num</label> <br>  
-                <input type="text" placeholder="Ex. 432">  <br> <br>  
+                <input type="text" name="num2" placeholder="Ex. 432">  <br> <br>  
+
+                <label>Operator</label> <br>  
+                <input type="text" name="op" placeholder="Ex. +, *, / or -">  <br> <br>  
+
                 <input type="submit" value="Submit">  
+
             </form>  
             ';
 
             $ageCalculator = '    
-            <form>  
+            <form method="get">  
+
                 <label>Enter Age </label> <br>  
-                <input type="text" placeholder="21"> <br> <br>  
+                <input type="text" name="age" placeholder="Ex. 21"> <br> <br>  
+
                 <label>Enter Year</label> <br>  
-                <input type="text" placeholder="1993">  <br> <br>  
+                <input type="text" name="year" placeholder="Ex. 1993">  <br> <br>  
+
                 <input type="submit" value="Submit">  
+
             </form>  
             ';
-
-    if($_GET["page"] == "calc"){
-        echo $calculator;
-
-
-    }elseif($_GET["page"] == "ageCalc"){
-        echo $ageCalculator;
-
-    }else{
+            
+    if(isset($_GET["page"]) && $_GET["page"] == "calc"){
+                echo $calculator;
+                if (isset($_GET["num1"]) && isset($_GET["num2"]) && isset($_GET["op"])) {
+                    $num1 = $_GET["num1"];
+                    $num2 = $_GET["num2"];
+                    $op = $_GET["op"];
+                    echo "First number > {$num1} {$op} {$num2} = " . ($num1 . $op . $num2);
+                }
+    } elseif(isset($_GET["page"]) && $_GET["page"] == "ageCalc"){
+                echo $ageCalculator;
+                if (isset($_GET["age"]) && isset($_GET["year"])) {
+                    $age = $_GET["age"];
+                    $year = $_GET["year"];
+                }
+    } else{
         echo "No page found...";
     }
+
 
         ?>
 </body>
