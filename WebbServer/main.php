0a31558f5bbfbebec945b2d4bb9b9639f87ee7e4
DontJudge <33662924+JakeWallbets@users.noreply.github.com>
2023-02-03 12:30:00 +0100

    Add files via upload

--- WebbServer/main.php
+++ WebbServer/main.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
@@ -16,57 +17,138 @@
         </nav>
     </header>
 
-        <?php
-            $calculator = '
+
+    <?php
+
+
+    $errorPage = '
+        <div class="container">
+            <p> No page found...  <br> </p>   
+            <p> Please verify your network connection or answers. </p>
+        </div> ';
+
+    $home = '
+            <div class="anwser">
+        </div>
+        <div class="container">  
             <form method="get">  
-
-                <label>First num </label> <br>  
-                <input type="text" name="num1" placeholder="Ex. 305"> <br> <br> 
-
-                <label>Second num</label> <br>  
-                <input type="text" name="num2" placeholder="Ex. 432">  <br> <br>  
-
-                <label>Operator</label> <br>  
-                <input type="text" name="op" placeholder="Ex. +, *, / or -">  <br> <br>  
-
-                <input type="submit" value="Submit">  
-
-            </form>  
-            ';
-
-            $ageCalculator = '    
-            <form method="get">  
-
-                <label>Enter Age </label> <br>  
-                <input type="text" name="age" placeholder="Ex. 21"> <br> <br>  
-
-                <label>Enter Year</label> <br>  
-                <input type="text" name="year" placeholder="Ex. 1993">  <br> <br>  
-
-                <input type="submit" value="Submit">  
-
-            </form>  
-            ';
             
-    if(isset($_GET["page"]) && $_GET["page"] == "calc"){
-                echo $calculator;
-                if (isset($_GET["num1"]) && isset($_GET["num2"]) && isset($_GET["op"])) {
-                    $num1 = $_GET["num1"];
-                    $num2 = $_GET["num2"];
-                    $op = $_GET["op"];
-                    echo "First number > {$num1} {$op} {$num2} = " . ($num1 . $op . $num2);
-                }
-    } elseif(isset($_GET["page"]) && $_GET["page"] == "ageCalc"){
-                echo $ageCalculator;
-                if (isset($_GET["age"]) && isset($_GET["year"])) {
-                    $age = $_GET["age"];
-                    $year = $_GET["year"];
-                }
-    } else{
-        echo "No page found...";
+            <label>First num </label> <br>  
+            <input type="number" name="num1" placeholder="Ex. 305"> <br> <br> 
+            
+            <label>Second num</label> <br>  
+                    <input type="number" name="num2" placeholder="Ex. 432">  <br> <br>
+                    
+                    <label>Operator</label> <br>  
+                    <select name="op">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                    </select>  <br> <br>  
+                    <input type="submit" value="Submit">  
+                    </form>
+                    </div>  
+                    
+                    ';
+
+
+    $calculator = '
+                    <div class="anwser">
+                    
+                    </div>
+                    <div class="container">  
+                    <form method="get">  
+                    <input type="hidden" name="page" value="calc">
+                    <label>First num </label> <br>  
+                    <input type="number" name="num1" placeholder="Ex. 305"> <br> <br> 
+                    
+                <label>Second num</label> <br>  
+                    <input type="number" name="num2" placeholder="Ex. 432">  <br> <br>
+                    
+                    <label>Operator</label> <br>  
+                    <select name="op">
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                    </select>  <br> <br>  
+                    <input type="submit" value="Submit">  
+                    </form>
+                    </div>  
+                    ';
+
+
+
+    $ageCalculator = ' 
+                    <div class="container">   
+                    <form method="get"> 
+                    
+                    <label>Enter Age </label> <br>  
+                    <input type="number" name="age" placeholder="Ex. 21"> <br> <br>  
+                    
+                    <label>Enter Year</label> <br>  
+                    <input type="number" name="year" placeholder="Ex. 1993">  <br> <br>  
+                    <input type="submit" value="Submit">  
+                    </form>  
+                    </div>
+                    ';
+
+
+    function giveResponseCalc($num1, $num2, $op)
+    {
+        global $sum;
+
+        switch ($op) {
+            case "+":
+                $sum = $num1 + $num2;
+                break;
+            case "-":
+                $sum = $num1 - $num2;
+                break;
+            case "/":
+                $sum = $num1 / $num2;
+                break;
+            case "*":
+                $sum = $num1 * $num2;
+                break;
+            default:
+                echo "Error Switch"; //Switch
+                break;
+        }
+        return $sum;
+    }
+
+    if (isset($_GET["page"])) {
+        if ($_GET["page"] == "calc") {
+            echo $calculator;
+            if (isset($_GET["op"]) && isset($_GET["num1"]) && isset($_GET["num2"])) {
+                echo giveResponseCalc($_GET['num1'], $_GET['num2'], $_GET['op']);
+            } else {
+                echo "Enter all numbers and oporators!";
+            }
+        } elseif ($_GET["page"] == "ageCalc") {
+            echo $ageCalculator;
+            if (isset($_GET["age"]) && isset($_GET["year"])) {
+                $age = $_GET["age"];
+                $year = $_GET["year"];
+            } else {
+                echo "Enter all numbers and oporators!";
+            }
+        }
+    } else {
+
     }
 
 
-        ?>
+    // Advanced miniräknare
+
+
+    
+
+
+    ?>
+
 </body>
+
 </html>
